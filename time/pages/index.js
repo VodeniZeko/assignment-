4fb2979fa4189d5d95897a88a9d3c3057bc8e8ea
VodeniZeko <edvin.saletovic@pm.me>
2020-12-30 02:22:16 -0800

    clock

---
+++ time/pages/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Popover from "./popover";
+
+/////////////////////////////////////////////////////////////////////
+
+export default function Time() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Time Assignment</title>
+        <link rel="icon" href="/clock.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <Popover />
+      </main>
+
+      <footer className={styles.footer}>
+        <a href="https://takija.io" target="_blank" rel="noopener noreferrer">
+          Created by <img src="/me.png" alt="image" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  );
+}
